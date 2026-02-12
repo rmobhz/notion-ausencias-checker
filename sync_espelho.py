d7db30d52c16b78ccaff048d5e5f9f075409e1a6
rmobhz <rmobhz@gmail.com>
2026-02-12 13:21:51 -0300

    Refactor sync_espelho.py with optional properties

---
+++ sync_espelho.py
@@ -0,0 +1,6 @@
+          DATABASE_ID_REUNIOES_ESPELHO: ${{ secrets.DATABASE_ID_REUNIOES_ESPELHO }}
+
+          # opcionais (só se no seu Notion tiver nomes diferentes)
+          PROP_RELACAO: "Origem"
+          PROP_TITULO_ESPELHO: "Name"
+          ORIGEM_TITLE_PROP: "Evento"
